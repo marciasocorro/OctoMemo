4653baa2f57b87ba183fcaa2fda19f8dd82ffa73
leonardofurtado <srleonardofurtado@gmail.com>
2019-10-19 19:49:24 -0300

    add sample test

---
+++ tests/test_user.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+from scripts.user import user
+
+
+class TestUser(TestCase):
+    def setUp(self):
+        TestCase.setUp(self)
+
+    def test_example(self):
+        self.assertTrue(True)
