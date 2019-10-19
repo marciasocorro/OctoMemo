e2a3e50cd0ff7ed50b87668f693a0dc85ad3cc1b
leonardofurtado <srleonardofurtado@gmail.com>
2019-10-19 19:56:30 -0300

    testing travis.

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
-from scripts.user import user
+from scripts.user import User
 
 
 class TestUser(TestCase):
     def setUp(self):
         TestCase.setUp(self)
 
-    def test_example(self):
+    def test_create_repo(self):
         self.assertTrue(True)
